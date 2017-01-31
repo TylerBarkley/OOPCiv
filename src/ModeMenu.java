6f1a2646671eba741cafc5c719b95152fbb1857f
hkerins <hkerins@ufl.edu>
2017-01-31 12:30:42 -0500

    progress on menus

---
+++ src/ModeMenu.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hankerins on 1/31/17.
+ */
+public class ModeMenu {
+
+}
---
+++ src/RallyPointMenu.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by hankerins on 1/31/17.
+ */
+public class RallyPointMenu {
+    ArrayList<RallyPoint> RallyPointList;
+    ArrayList<RallyPointInstruction> RallyPointInstructions;
+
+    RallyPointMenu(){
+        //Add instructions to instruction arraylist
+
+    }
+}
