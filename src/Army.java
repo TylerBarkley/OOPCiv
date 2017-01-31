10795ad8ebe79f3839fe674afdca320e7a7079a0
hkerins <hkerins@ufl.edu>
2017-01-31 11:22:24 -0500

    small army change

--- src/Army.java
+++ src/Army.java
@@ -6,4 +6,8 @@ import java.util.ArrayList;
 public class Army extends Controllable {
     BattleGroup battleGroup;
     Reinforcements reinforcements;
+
+    void move(char cardinalDirection){
+
+    }
 }
