388d17d8efce0ea65a4ad7129dade7092aa17d43
TylerBarkley <tylerb306@ufl.edu>
2017-02-02 14:15:43 -0500

    Keyz

--- src/MeleeFighter.java
+++ src/MeleeFighter.java
@@ -2,7 +2,7 @@
  * Created by Trevor on 1/31/2017.
  */
 public class MeleeFighter extends Unit {
-    MeleeFighter(String unitType,Tile tileCreatedOn, Map currentMap){
-        super(unitType,tileCreatedOn, currentMap);
+    MeleeFighter(String unitType, Map currentMap){
+        super(unitType, currentMap);
     };
 }
