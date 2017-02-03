48378026e2fbe8ea364d8955622151c743ba362f
Joshua Novick <jnovick@ufl.edu>
2017-02-02 22:57:52 -0500

    Unit Overview helper methods added

--- src/UnitOverview.java
+++ src/UnitOverview.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class UnitOverview extends Overview{
 
@@ -6,8 +7,32 @@ public class UnitOverview extends Overview{
 
 	}
 	
-	private Unit[] getAllUnits(){
-		return null;
+	private ArrayList<Unit> getAllUnits(){
+		ArrayList<Unit> allUnits=new ArrayList<Unit>();
+		for(ArrayList<Unit> units: player.getUnits()){
+			allUnits.addAll(units);
+		}
+		return allUnits;
+	}
+	
+	private ArrayList<Unit> getExplorerUnits(){
+		return player.getUnits().get(GameInfo.EXPLORER);	
+	}
+	
+	private ArrayList<Unit> getColonistUnits(){
+		return player.getUnits().get(GameInfo.COLONIST);
+	}
+	
+	private ArrayList<Unit> getMeleeFighterUnits(){
+		return player.getUnits().get(GameInfo.MELEEUNIT);
+	}
+	
+	private ArrayList<Unit> getRangedFighterUnits(){
+		return player.getUnits().get(GameInfo.RANGEDUNIT);
+	}
+	
+	private ArrayList<Army> getArmies(){
+		return player.getArmies();
 	}
 	
 	private void displayUnitStatus(Unit unit){
@@ -16,7 +41,6 @@ public class UnitOverview extends Overview{
 
 	public void updateView() {
 
-		
 	}
 
 	public void displayView() {
