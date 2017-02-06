f60a2a8418cc6450e3b194c3df053e59847eecb5
TylerBarkley <tylerb306@ufl.edu>
2017-02-05 23:15:33 -0500

    Added real stats and armor

--- src/StatsFactory.java
+++ src/StatsFactory.java
@@ -11,38 +11,44 @@ public class StatsFactory {
         Stats concreteTypeStats=new Stats();
         //MELEEFIGHTER
         if(concreteType==GameInfo.MELEEUNIT) {
-            concreteTypeStats.setDefensiveDamage(0);
-            concreteTypeStats.setOffensiveDamage(0);
-            concreteTypeStats.setHealth(0);
-            concreteTypeStats.setUpKeep(0);
-            concreteTypeStats.setRate(0);
+            concreteTypeStats.setDefensiveDamage(2);
+            concreteTypeStats.setArmor(2);
+            concreteTypeStats.setOffensiveDamage(4);
+            concreteTypeStats.setHealth(4);
+            concreteTypeStats.setUpKeep(3);
+            concreteTypeStats.setRate(2);
         } else if(concreteType==GameInfo.RANGEDUNIT) {
-            concreteTypeStats.setDefensiveDamage(0);
-            concreteTypeStats.setOffensiveDamage(0);
-            concreteTypeStats.setHealth(0);
-            concreteTypeStats.setUpKeep(0);
-            concreteTypeStats.setRate(0);
+            concreteTypeStats.setDefensiveDamage(1);
+            concreteTypeStats.setArmor(1);
+            concreteTypeStats.setOffensiveDamage(4);
+            concreteTypeStats.setHealth(3);
+            concreteTypeStats.setUpKeep(2);
+            concreteTypeStats.setRate(3);
         }  else  if(concreteType==GameInfo.COLONIST) {
-            concreteTypeStats.setDefensiveDamage(0);
-            concreteTypeStats.setOffensiveDamage(0);
-            concreteTypeStats.setHealth(0);
-            concreteTypeStats.setUpKeep(0);
-            concreteTypeStats.setRate(0);
+            concreteTypeStats.setDefensiveDamage(2);
+            concreteTypeStats.setArmor(1);
+            concreteTypeStats.setOffensiveDamage(2);
+            concreteTypeStats.setHealth(3);
+            concreteTypeStats.setUpKeep(2);
+            concreteTypeStats.setRate(2);
         } else if(concreteType==GameInfo.EXPLORER) {
-            concreteTypeStats.setDefensiveDamage(0);
-            concreteTypeStats.setOffensiveDamage(0);
-            concreteTypeStats.setHealth(0);
-            concreteTypeStats.setUpKeep(0);
-            concreteTypeStats.setRate(0);
+            concreteTypeStats.setDefensiveDamage(1);
+            concreteTypeStats.setArmor(0);
+            concreteTypeStats.setOffensiveDamage(2);
+            concreteTypeStats.setHealth(3);
+            concreteTypeStats.setUpKeep(1);
+            concreteTypeStats.setRate(4);
         } else if(concreteType== GameInfo.BASE){
-            concreteTypeStats.setDefensiveDamage(0);
-            concreteTypeStats.setOffensiveDamage(0);
-            concreteTypeStats.setHealth(0);
-            concreteTypeStats.setUpKeep(0);
-            concreteTypeStats.setRate(0);
+            concreteTypeStats.setDefensiveDamage(3);
+            concreteTypeStats.setArmor(3);
+            concreteTypeStats.setOffensiveDamage(4);
+            concreteTypeStats.setHealth(5);
+            concreteTypeStats.setUpKeep(4);
+            concreteTypeStats.setRate(3);
         }
         else if(concreteType== GameInfo.ARMY){
             concreteTypeStats.setDefensiveDamage(0);
+            concreteTypeStats.setArmor(0);
             concreteTypeStats.setOffensiveDamage(0);
             concreteTypeStats.setHealth(0);
             concreteTypeStats.setUpKeep(0);
